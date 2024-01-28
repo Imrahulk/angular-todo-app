64a43ee76c42eaf4433f3dc1730c43e088ca4c77
Imrahulk <ping2rahulk@gmail.com>
2024-01-28 22:08:00 +0530

    Code cleaning

--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -46,6 +46,7 @@ export class TodoComponent implements OnInit, OnChanges {
   }
   toggleTodo(): void {
     this.todoService.toggleTodo(this.todoProps.id);
+    console.log('toggleTodo');
   }
   changeText(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
